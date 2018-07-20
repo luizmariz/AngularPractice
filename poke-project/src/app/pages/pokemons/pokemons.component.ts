d65939d825fc320bf1825117f9c9475a73fcac65
Luiz Gustavo <luizlgom@gmail.com>
2018-07-20 15:40:15 -0300

    +pokemonpage

--- poke-project/src/app/pages/pokemons/pokemons.component.ts
+++ poke-project/src/app/pages/pokemons/pokemons.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+
 
 @Component({
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
   styleUrls: ['./pokemons.component.css']
 })
+
 export class PokemonsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public pkmnService: PokemonService) { }
 
   ngOnInit() {
   }
 
+  getPokemon(){
+    console.log("Entrei na função")
+    this.id = 1;
+    this.pkmnService.getPokemon(this.id)
+    .subscribe(
+        (res)=>{
+          this.pokemons.push(res);
+          console.log(res);
+        }
+    )
+  }
 }
