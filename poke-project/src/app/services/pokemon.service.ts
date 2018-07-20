4fd9177c1a426406fe014882be8e1f464044f689
Luiz Gustavo <luizlgom@gmail.com>
2018-07-20 20:44:12 -0300

    *Cards e logo

--- poke-project/src/app/services/pokemon.service.ts
+++ poke-project/src/app/services/pokemon.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
