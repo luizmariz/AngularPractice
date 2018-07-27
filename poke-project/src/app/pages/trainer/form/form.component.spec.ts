ea6835b2734f26fb98efbe311ea54285a9d37f02
Luiz <37978726+luizmariz@users.noreply.github.com>
2018-07-27 17:29:34 -0300

    +modal

--- poke-project/src/app/pages/trainer/form/form.component.spec.ts
+++ poke-project/src/app/pages/trainer/form/form.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BerriesComponent } from './berries.component';
+import { FormComponent } from './form.component';
 
-describe('BerriesComponent', () => {
-  let component: BerriesComponent;
-  let fixture: ComponentFixture<BerriesComponent>;
+describe('FormComponent', () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BerriesComponent ]
+      declarations: [ FormComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BerriesComponent);
+    fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
