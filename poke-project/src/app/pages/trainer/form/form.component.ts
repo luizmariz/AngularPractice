20236acc11fdd2f1bce93d95ec907578592dbf7c
Luiz <37978726+luizmariz@users.noreply.github.com>
2018-07-29 16:08:11 -0300

    +validators tudo show

--- poke-project/src/app/pages/trainer/form/form.component.ts
+++ poke-project/src/app/pages/trainer/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import { MaterializeAction } from "angular2-materialize";
 import { PokemonService } from '../../../services/pokemon.service';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-form',
@@ -14,9 +15,19 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  modalActions = new EventEmitter<string|MaterializeAction>();
+  abreModal() {
+  	this.modalActions.emit({action:"modal",params:['open']});
+  }
+
+  fechaModal() {
+  	this.modalActions.emit({action:"modal",params:['close']});
+  }
 
   inputDisabled: boolean = true;
 
+  /* Vamos cuidar para que o user nao escolha a cidade inicial 2x */
   checkbox = [false,false,false,false,false,false,false,false];
   i;
   disableCheckbox(posicao) {
@@ -25,6 +36,61 @@ export class FormComponent implements OnInit {
     }
 
     this.checkbox[posicao] = true;
+  }
+
+  /* Guardar as regioes selecionadas pelo user */
+  arrayRegioes = [];
+  getRegiao(x,y) {
+    if(x == true){
+      this.arrayRegioes.push(y);
+    }
+    
+    if(x == false){
+      this.arrayRegioes.splice(this.arrayRegioes.indexOf(y),1);
+    }
+  }
+
+  /*Checar a idade do user*/
+  ageError: boolean = false;
+  checkAge(idade){
+    if(idade.value < 10)
+      this.ageError = true;
+    
+    else 
+      this.ageError = false;
+    
+  }
+
+  /*Checar o numero de identificacao*/
+
+  numIdentError: boolean = false;
+  checkNumIdent(numIdent){
+    if((numIdent.value.length != 8) )
+      this.numIdentError = true;
+
+    else 
+      this.numIdentError = false;
+    
+  }
+  
+  /* Quem é esse pokemon! */
+
+  pokemon;
+  poke;
+  pokemonError: boolean = false;
+  getPokemon(poke){
+    console.log("Entrei na função")
+    this.pkmnService.getPokemon(poke.value.toLowerCase())
+    .subscribe(
+      (res)=>{
+        this.pokemon = res;
+        this.pokemonError = false;
+      },
+
+      (error) =>{
+        this.pokemonError = true;
+      }
+    )
   }
 
   onSubmit(jornada) {
@@ -36,50 +102,4 @@ export class FormComponent implements OnInit {
     console.log(jornada.value.poke);
     
   }
-
-  arrayRegioes = [];
-  essaBudegaMudou(x,y) {
-    if(x == true){
-      this.arrayRegioes.push(y);
-    }
-    
-    if(x == false){
-      this.arrayRegioes.splice(this.arrayRegioes.indexOf(y),1);
-    }
-  }
-
-  passwordError:boolean = false;
-
-  checkPassword(senha){
-  	if(senha.value.length < 6)
-  		this.passwordError = true;
- 
-  	else
-  		this.passwordError = false;
-  }
-
-  modalActions = new EventEmitter<string|MaterializeAction>();
-  abreModal() {
-  	this.modalActions.emit({action:"modal",params:['open']});
-  }
-
-  fechaModal() {
-  	this.modalActions.emit({action:"modal",params:['close']});
-  }
-
-  /* Quem é esse pokemon! */
-
-  pokemon;
-  poke;
-  
-  getPokemon(poke){
-    console.log("Entrei na função")
-    this.pkmnService.getPokemon(poke.value)
-    .subscribe(
-      (res)=>{
-        this.pokemon = res;
-      }
-    )
-  }
-
 }
